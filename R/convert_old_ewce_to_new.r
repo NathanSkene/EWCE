44a27dd8b90b9bf961974322c55f5a2995c860aa
nskene <nathan.skene@gmail.com>
2018-06-18 13:19:53 +0100

    Added the conversion scripts About to edit the controlled mapping
    Former-commit-id: 7f50d3712db148ef83d398c9ae1e8416c2528a4e

--- R/convert_old_ewce_to_new.r
+++ R/convert_old_ewce_to_new.r
@@ -12,14 +12,13 @@ convert_old_ewce_to_new <- function(level1,level2){
 }
 
 
-convert_new_ewce_to_old <- function(ctdIN){
-    ctd=list()
-    ctd[[1]]=list()
-    ctd[[1]]$specificity = celltype_data[[1]]$cell_dists
-    ctd[[1]]$mean_exp    = celltype_data[[1]]$all_scts
-    load(level2)
-    ctd[[2]]=list()
-    ctd[[2]]$specificity = celltype_data[[1]]$cell_dists
-    ctd[[2]]$mean_exp    = celltype_data[[1]]$all_scts
-    return(ctd)
+convert_new_ewce_to_old <- function(ctd,lvl){
+    celltype_data=list()
+    celltype_data[[1]]=list()
+    celltype_data[[1]]$cell_dists  = ctd[[lvl]]$specificity
+    celltype_data[[1]]$all_scts    = ctd[[lvl]]$mean_exp
+    #celltype_data[[2]]=list()
+    #celltype_data[[2]]$cell_dists = ctd[[2]]$specificity
+    #celltype_data[[2]]$all_scts   = ctd[[2]]$mean_exp
+    return(celltype_data)
 }
